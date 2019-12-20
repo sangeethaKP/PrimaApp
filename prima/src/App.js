389d2b73bcbe8fa7e25a9ddfa1f4b70f9d352ac3
Sangeetha Krishna Pillai <sangeep@amazon.com>
2019-12-19 18:03:20 -0800

    Set up Google Authentication

--- prima/src/App.js
+++ prima/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { LandingPage } from './LandingPage'
 
 function App () {
   return (
     <div>
-      Hello Prima!
+      <LandingPage />
     </div>
   )
 }
---
+++ prima/src/LandingPage.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react"
+import firebase from "firebase"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC2xQEtycC3dGsUuIz6gaTNW1j6hn335HA",
+  authDomain: "prima-b940d.firebaseapp.com"
+})
+console.log('initializing firebase here')
+
+export class LandingPage extends Component {
+  state = { isSignedIn: false }
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user })
+      console.log('component did mount updating isSignedIn state here')
+      console.log("user", user)
+    })
+  }
+
+  render() {
+    console.log('trying to render here')
+    return (
+      <div>
+        {this.state.isSignedIn ? (
+          <span>
+            <div>Signed In!</div>
+            <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+            <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+          </span>
+        ) : (
+          <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        )}
+      </div>
+    )
+  }
+}
