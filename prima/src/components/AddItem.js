47559e313f44d43d9fd9fa278ef05c8595080de4
Sangeetha Krishna Pillai <sangeep@amazon.com>
2019-12-26 18:08:10 -0800

    Refactor add item to it's own component

---
+++ prima/src/components/AddItem.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {addToDo} from '../store/actions'
+
+class AddItem extends Component {
+
+  state = {
+    title: "",
+    importance: false,
+    urgency: false
+  };
+
+  formSubmit = event => {
+    const {title, urgency, importance} = this.state;
+    const {addItem} = this.props;
+    event.preventDefault();
+    addItem({title: title, urgent: urgency ? 1 : 0, important: importance ? 1 : 0});
+    this.setState({title: "", urgency: 0, importance: 0});
+  };
+
+  titleChange = event => {
+    this.setState({...this.state, title: event.target.value});
+  }
+
+  urgencyChange = event => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({...this.state, urgency: value});
+  }
+
+  importanceChange = event => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({...this.state, importance: value});
+  }
+
+  renderForm = () => {
+    const {title, urgency, importance} = this.state;
+      return (
+        <div>
+          <form>
+            <div>
+              <input value={title} onChange={this.titleChange} type="text"/>
+              <input name="Urgency" type="checkbox" checked={this.state.urgency} onChange={this.urgencyChange} /> Urgent
+              <input name="Importance" type="checkbox" checked={this.state.importance} onChange={this.importanceChange} /> Important
+              <br />
+              <br />
+              <button text = "SUBMIT" onClick={this.formSubmit} > SUBMIT </button>
+            </div>
+          </form>
+        </div>
+      );
+  };
+
+  render () {
+    return (
+      <div>{this.renderForm()}</div>
+    )
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addItem: (todo) => {
+      dispatch(addToDo(todo))
+    }
+  }
+}
+
+export default connect(null, mapDispatchToProps)(AddItem)
--- prima/src/components/List.js
+++ prima/src/components/List.js
@@ -1,76 +1,44 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import _ from 'lodash';
-import {fetchToDos, addToDo} from '../store/actions';
-import ListItem from './ListItem';
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import _ from 'lodash'
+import {fetchToDos, addToDo} from '../store/actions'
+import ListItem from './ListItem'
+import AddItem from './AddItem'
 
 class List extends Component {
-  state = {
-    formValue: ""
-  };
-
-  inputChange = event => {
-    this.setState({formValue: event.target.value});
-  };
-
-  formSubmit = event => {
-    const {formValue} = this.state;
-    const {addItem} = this.props;
-    event.preventDefault();
-    addItem({title: formValue});
-    this.setState({formValue: ""});
-  };
-
-  renderForm = () => {
-    const {formValue} = this.state;
-      return (
-        <div>
-          <form onSubmit={this.formSubmit}>
-            <div>
-              <input 
-                value={formValue}
-                onChange={this.inputChange}
-                type="text"
-              />
-              <label> What Next?</label>
-            </div>
-          </form>
-        </div>
-      );
-  };
-  renderToDo() {
-    const {data} = this.props;
+  renderToDo () {
+    const {data} = this.props
     const toDos = _.map(data, (value, key) => {
-      return <ListItem key={key} todoId={key} todo={value} />;
-    });
+      return <ListItem key={key} todoId={key} todo={value} />
+    })
     if (!_.isEmpty(toDos)) {
-      return toDos;
+      return toDos
     }
     return (
       <div><h4>You have no more things ToDo!</h4></div>
-    );
+    )
   }
-  componentWillMount() {
-    this.props.listItems();
+  componentWillMount () {
+    this.props.listItems()
   }
-  render() {
+  render () {
     return (
       <div>
         <div>
-          {this.renderForm()}
+          <AddItem />
           {this.renderToDo()}
         </div>
       </div>
-    );
+    )
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    addItem : (todo) => {
+    addItem: (todo) => {
       dispatch(addToDo(todo))
     },
-    listItems : () => {
+    listItems: () => {
       dispatch(fetchToDos())
     }
   }
@@ -82,4 +50,4 @@ const mapStateToProps = ({data}) => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(List);
+export default connect(mapStateToProps, mapDispatchToProps)(List)
